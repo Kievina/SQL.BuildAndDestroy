5ae4e3d8ebb3c09a582da7abfe0cc80a685d1e22
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-25 20:45:00 -0500

    all queries complete

---
+++ answers/exercise3.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE myNewDB;
+CREATE TABLE Users (
+	UserID int,
+    LastName VARCHAR(255),
+    FirstName VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255)
+    );
+    
+	
